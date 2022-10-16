94293919ec57ce7c87886aa3e25221c71f2fc72c
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2022-10-16 17:00:49 +0800

    create a MainLogic

--- src/Service/ServiceMainLogic.java
+++ src/Service/ServiceMainLogic.java
@@ -3,6 +3,6 @@ package Service;
 import Dao.ServiceToDaoInterface;
 
 public class ServiceMainLogic {
-    public static ServiceToDaoInterface serviceToDaoInterface;//在整个项目的主逻辑里赋值
+    public static ServiceToDaoInterface serviceToDaoInterface;//接口入口，在整个项目的主逻辑里赋值
 
 }
---
+++ src/UI/UIMainLogic.java
@@ -0,0 +1,8 @@
+package UI;
+
+import Service.UIToServiceInterface;
+
+public class UIMainLogic {
+    public static UIToServiceInterface uiToServiceInterface;//接口入口，在整个项目的主逻辑里赋值
+
+}
