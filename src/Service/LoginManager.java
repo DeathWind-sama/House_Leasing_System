d28ab256e0727bc553231e9ca24c515b7dda0e0a
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2022-10-17 21:53:03 +0800

    make login better

--- src/Service/LoginManager.java
+++ src/Service/LoginManager.java
@@ -20,19 +20,10 @@ public class LoginManager {
         System.out.println("Login...");
         //使用异步以把主进程留给其他请求
         new Thread(() -> {
-            try {
-                AtomicBoolean isSuccess = new AtomicBoolean(false);
-                //创建新进程以插队运行等待请求回复
-                Thread thread = new Thread(() -> {
-                    //请求数据库进行匹配
-                    isSuccess.set(ServiceMainLogic.serviceToDaoInterface.matchUserToLogin(ID, psw));
-                });
-                thread.start();
-                thread.join();//匹配过程中阻塞当前进程
-                checkLoginSuccess(isSuccess.get());//登录操作完成，根据是否成功进行分支
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            AtomicBoolean isSuccess = new AtomicBoolean(false);
+            //创建新进程以插队运行等待请求回复
+            isSuccess.set(ServiceMainLogic.serviceToDaoInterface.matchUserToLogin(ID, psw));
+            checkLoginSuccess(isSuccess.get());//登录操作完成，根据是否成功进行分支
         }).start();
     }
 
