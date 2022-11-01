a6b0d262d5f106bc4715dc12c9b08f51fb0238a8
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2022-11-01 13:59:25 +0800

    jsp-api and servlet-api added

---
+++ src/main/java/service/Login.java
@@ -0,0 +1,11 @@
+package service;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class Login {
+
+}
