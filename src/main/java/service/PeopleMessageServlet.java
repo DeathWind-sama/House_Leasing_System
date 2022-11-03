6ecbc00784104a5081bb2bdea707ce44eeb33698
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2022-11-03 21:22:17 +0800

    PeopleMessageServlet done

--- src/main/java/service/PeopleMessageServlet.java
+++ src/main/java/service/PeopleMessageServlet.java
@@ -1,13 +1,20 @@
 package service;
 
 import com.alibaba.fastjson2.JSONArray;
+import dao.ServiceToDaoInterface;
+import dao.ServiceToDaoRealization;
+import object.People;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+
+import static com.alibaba.fastjson2.JSONObject.toJSONString;
 
 @WebServlet(name = "PeopleMessage", value = "/PeopleMessage")
 public class PeopleMessageServlet extends HttpServlet {
@@ -19,11 +26,27 @@ public class PeopleMessageServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Servlet: POST to PeopleMessage");
-        Map<String, String> responseMap = new HashMap<>();
-        JSONArray responseJSArray = new JSONArray();
 
         String identity = request.getParameter("identity");
         String id = request.getParameter("id");
 
+        People people=new People();
+
+        ServiceToDaoInterface serviceToDaoInterface=new ServiceToDaoRealization();
+        boolean isSucceed=serviceToDaoInterface.getPeople(id, Objects.equals(identity, "homeowner"),people);
+
+        if(isSucceed){
+            System.out.println("Succeed");
+
+            String responseJSStr = toJSONString(people);
+            System.out.println("Response JS: " + responseJSStr);
+
+            //response
+            PrintWriter responseWriter = response.getWriter();
+            responseWriter.write(responseJSStr);
+        }else{
+            System.out.println("Fail");
+            response.setStatus(404);
+        }
     }
 }
